63e0b9d980fbc98ee93939eaacccbe285f9ec769
Andrey Derevyanko <derandr@gmail.com>
2018-03-23 18:17:38 +0100

    Location request service added. Not tested yet

---
+++ app/src/main/java/com/starcon/master/locator/LocationService.java
@@ -0,0 +1,234 @@
+package com.starcon.master.locator;
+
+import android.app.Service;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.security.GeneralSecurityException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public class LocationService extends Service implements LocationListener {
+
+    boolean isGPSEnable = false;
+    boolean isNetworkEnable = false;
+    double latitude, longitude;
+    LocationManager locationManager;
+    Location location;
+    private Handler mHandler = new Handler();
+    private Timer mTimer = null;
+    public static String str_receiver = "location.service.receiver";
+    Intent intent;
+
+    private int mUpdateInterval;
+    private String mServerName;
+    private RequestQueue mRequestQueue;
+    private String mBfPassword;
+    private String mMyId;
+    private StringBuilder mOtherResult;
+
+
+    public LocationService() {
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        SharedPreferences mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mUpdateInterval = Integer.valueOf(mSharedPreferences.getString("pref_sync_time", "10")) * 1000;
+        mServerName = mSharedPreferences.getString("pref_server", "http://derandr.000webhostapp.com");
+        mBfPassword = mSharedPreferences.getString("pref_pass", "SomeTempPassword");
+        mMyId = mSharedPreferences.getString("pref_name", "User");
+
+        mRequestQueue = Volley.newRequestQueue(this);
+
+        mTimer = new Timer();
+        mTimer.schedule(new TimerTaskToGetLocation(), 5, mUpdateInterval);
+        intent = new Intent(str_receiver);
+        mOtherResult = new StringBuilder();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    private void serviceGetLocation() {
+        locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+        isGPSEnable = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        isNetworkEnable = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        if (isNetworkEnable) {
+            location = null;
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 0, this);
+            if (locationManager != null) {
+                location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                if (location != null) {
+
+                    Log.e("latitude", location.getLatitude() + "");
+                    Log.e("longitude", location.getLongitude() + "");
+
+                    latitude = location.getLatitude();
+                    longitude = location.getLongitude();
+                    serviceDataUpdate(location);
+                }
+            }
+
+        } else if (isGPSEnable) {
+            location = null;
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, this);
+            if (locationManager != null) {
+                location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                if (location != null) {
+                    Log.e("latitude", location.getLatitude() + "");
+                    Log.e("longitude", location.getLongitude() + "");
+                    latitude = location.getLatitude();
+                    longitude = location.getLongitude();
+                    serviceDataUpdate(location);
+                }
+            }
+        }
+
+
+    }
+
+    private class TimerTaskToGetLocation extends TimerTask {
+        @Override
+        public void run() {
+
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    serviceGetLocation();
+                }
+            });
+
+        }
+    }
+
+    private void serviceDataUpdate(Location location) {
+        String my_location = String.valueOf(location.getLatitude()) + ";" + String.valueOf(location.getLongitude());
+        intent.putExtra("me", my_location);
+        String other_result = mOtherResult.toString();
+        if (!other_result.isEmpty()) intent.putExtra("other", other_result);
+        sendBroadcast(intent);
+        sendLocation(my_location);
+        getLocation();
+    }
+
+    void sendLocation(String location) {
+        if (mMyId.equals("User")) return;
+        byte[] encoded_id = new byte[0];
+        byte[] encoded_location = new byte[0];
+        try {
+            encoded_id = LocationSecurity.encrypt(mBfPassword, mMyId);
+            encoded_location = LocationSecurity.encrypt(mBfPassword, location);
+        } catch (GeneralSecurityException e) {
+            e.printStackTrace();
+        }
+        String url = mServerName + "?v1=" + LocationSecurity.byteArrayToHexString(encoded_id) + "&&v2=" + LocationSecurity.byteArrayToHexString(encoded_location);
+
+        StringRequest request = new StringRequest(com.android.volley.Request.Method.GET, url,
+                new com.android.volley.Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // No action
+                    }
+                }, new com.android.volley.Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // No action
+            }
+        });
+        mRequestQueue.add(request);
+    }
+
+    void getLocation() {
+
+        String url = mServerName + "?v1=query";
+
+        Response.Listener<JSONArray> listener = new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject object = response.getJSONObject(i);
+
+                        String decodedID = "";
+                        String decodedLocation = "";
+                        try {
+                            decodedID = LocationSecurity.decrypt(mBfPassword, LocationSecurity.hexStringToByteArray(object.getString("N")));
+                            decodedLocation = LocationSecurity.decrypt(mBfPassword, LocationSecurity.hexStringToByteArray(object.getString("C")));
+                        } catch (GeneralSecurityException e) {
+                            e.printStackTrace();
+                        }
+
+                        if (!decodedID.equals(mMyId)) {
+                            Log.d("Record: ", "N: " + decodedID + " C: " + decodedLocation);
+                            String[] data = decodedLocation.split(";");
+                            mOtherResult.append(decodedID).append("+").append(data[0]).append(";").append(data[1]).append(":");
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+
+        Response.ErrorListener error_listener = new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // TODO: Handle error
+            }
+        };
+
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, "", listener, error_listener);
+        mRequestQueue.add(request);
+
+    }
+
+}
--- app/src/main/java/com/starcon/master/locator/MapsActivity.java
+++ app/src/main/java/com/starcon/master/locator/MapsActivity.java
@@ -1,38 +1,19 @@
 package com.starcon.master.locator;
 
-import android.Manifest;
+import android.app.ActivityManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
-import android.content.IntentSender;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.os.Handler;
 import android.preference.PreferenceManager;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 import android.support.v7.widget.Toolbar;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.github.pengrad.mapscaleview.MapScaleView;
-import com.google.android.gms.common.api.ResolvableApiException;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.LocationSettingsResponse;
-import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -42,42 +23,29 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.security.GeneralSecurityException;
 import java.util.HashMap;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback,
         GoogleMap.OnCameraMoveListener,
-GoogleMap.OnCameraIdleListener,
-GoogleMap.OnCameraChangeListener,
-GoogleMap.OnMarkerClickListener,
-GoogleMap.OnMapClickListener{
+        GoogleMap.OnCameraIdleListener,
+        GoogleMap.OnCameraChangeListener,
+        GoogleMap.OnMarkerClickListener,
+        GoogleMap.OnMapClickListener {
 
     private final int REQUEST_CHECK_SETTINGS = 0;
 
     private GoogleMap mMap;
     private MapScaleView mScaleView;
 
-    private FusedLocationProviderClient mLocationClient;
-    private int mUpdateInterval;
-    private Handler mRunHandler;
-    private RequestQueue mRequestQueue;
     private HashMap<String, Marker> mMarkers = new HashMap<>();
 
     private String mMyId;
     private Marker mMyMarker;
     private Marker mActiveMarker;
-    private String mServerName;
-    private String mBfPassword;
+    private Intent mLocationIntent;
 
-    private boolean mAutoUpdate = false;
+    private boolean mServiceRunning = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -88,58 +56,60 @@ GoogleMap.OnMapClickListener{
                 .findFragmentById(R.id.map);
 
         Toolbar mToolbar = findViewById(R.id.my_toolbar);
-        mLocationClient = LocationServices.getFusedLocationProviderClient(this);
-
 
         SharedPreferences sharedPref_ = PreferenceManager.getDefaultSharedPreferences(this);
         mMyId = sharedPref_.getString("pref_name", "User");
-        mBfPassword = sharedPref_.getString("pref_pass", "SomeTempPassword");
-        mUpdateInterval = Integer.valueOf(sharedPref_.getString("pref_sync_time", "10")) * 1000;
-        mServerName = sharedPref_.getString("pref_server", "http://derandr.000webhostapp.com");
+
+        mServiceRunning = isServiceRunning(LocationService.class);
+
+        mLocationIntent = new Intent(getApplicationContext(), LocationService.class);
 
         setSupportActionBar(mToolbar);
 
-        createLocationRequest();
-
-        mRunHandler = new Handler();
-        mRequestQueue = Volley.newRequestQueue(this);
         mapFragment.getMapAsync(this);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        SharedPreferences sharedPref_ = PreferenceManager.getDefaultSharedPreferences(this);
-        mMyId = sharedPref_.getString("pref_name", "User");
-        mBfPassword = sharedPref_.getString("pref_pass", "SomeTempPassword");
-        mUpdateInterval = Integer.valueOf(sharedPref_.getString("pref_sync_time", "10")) * 1000;
-        mServerName = sharedPref_.getString("pref_server", "http://derandr.000webhostapp.com");
-        if (mMyMarker != null) mMyMarker.setTitle(mMyId);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.toolbar, menu);
         MenuItem menuItem = menu.findItem(R.id.action_auto);
-        menuItem.setChecked(false);
+        menuItem.setChecked(mServiceRunning);
+        menuItem.setIcon(mServiceRunning ? R.drawable.ic_auto_pressed : R.drawable.ic_auto_update);
         return true;
+    }
+
+
+    private boolean isServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_connect:
-                updateStatus();
+                //updateStatus();
                 return true;
             case R.id.action_settings:
                 Intent intent = new Intent(this, SettingsActivity.class);
                 startActivity(intent);
                 return true;
             case R.id.action_auto:
-                mAutoUpdate = !mAutoUpdate;
-                item.setChecked(mAutoUpdate);
-                item.setIcon(mAutoUpdate ? R.drawable.ic_auto_pressed : R.drawable.ic_auto_update);
-                if (mAutoUpdate) startRepeatingTask();
+                mServiceRunning = !mServiceRunning;
+                item.setChecked(mServiceRunning);
+                item.setIcon(mServiceRunning ? R.drawable.ic_auto_pressed : R.drawable.ic_auto_update);
+                if (mServiceRunning && !isServiceRunning(LocationService.class)) {
+                        startService(mLocationIntent);
+                } else {
+                    if (isServiceRunning(LocationService.class)) {
+                        stopService(mLocationIntent);
+                    }
+                }
                 return true;
             default:
                 // If we got here, the user's action was not recognized.
@@ -194,155 +164,48 @@ GoogleMap.OnMapClickListener{
         mActiveMarker = null;
     }
 
-    void updateStatus() {
-        if (ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-        mLocationClient.getLastLocation()
-                .addOnSuccessListener(MapsActivity.this, new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        if (location != null) {
-                            mMyMarker.setPosition(new LatLng(location.getLatitude(), location.getLongitude()));
-                            sendLocation(String.valueOf(location.getLatitude()) + ";" + String.valueOf(location.getLongitude()));
-                            getLocation();
-                        }
-                    }
-                });
-        if (mActiveMarker != null) mMap.animateCamera(CameraUpdateFactory.newLatLng(mActiveMarker.getPosition()));
-    }
-
-    protected void createLocationRequest() {
-        LocationRequest locationRequest = new LocationRequest();
-        locationRequest.setInterval(mUpdateInterval);
-        locationRequest.setFastestInterval(mUpdateInterval);
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-
-        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
-                .addLocationRequest(locationRequest);
-
-        SettingsClient client = LocationServices.getSettingsClient(this);
-        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
-
-
-        task.addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
-            @Override
-            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
-                Toast.makeText(MapsActivity.this, "Settings OK", Toast.LENGTH_SHORT).show();
+    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.hasExtra("me")) {
+                String my_location = intent.getStringExtra("me");
+                String[] data = my_location.split(";");
+                mMyMarker.setPosition(new LatLng(Double.valueOf(data[0]), Double.valueOf(data[1])));
             }
-        });
-
-        task.addOnFailureListener(this, new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                if (e instanceof ResolvableApiException) {
-                    try {
-                        ResolvableApiException resolvable = (ResolvableApiException) e;
-                        resolvable.startResolutionForResult(MapsActivity.this,
-                                REQUEST_CHECK_SETTINGS);
-                    } catch (IntentSender.SendIntentException sendEx) {
-                        // Ignore the error.
+            if (intent.hasExtra("other")) {
+                String other_location = intent.getStringExtra("other");
+                String[] records = other_location.split(":");
+                for (int i = 0; i < records.length - 1; ++i) {
+                    String[] record = records[i].split("\\+");
+                    String[] coords = record[1].split(";");
+                    Marker marker = mMarkers.get(record[0]);
+                    if (marker == null) {
+                        mMarkers.put(record[0], mMap.addMarker(new MarkerOptions()
+                                .position(new LatLng(Double.valueOf(coords[0]), Double.valueOf(coords[1])))
+                                .title(record[0])
+                                .icon(BitmapDescriptorFactory.defaultMarker(MarkerProperties.MarkerColourByIndex(i)))));
+                    } else {
+                        marker.setPosition(new LatLng(Double.valueOf(coords[0]), Double.valueOf(coords[1])));
                     }
                 }
             }
-        });
-
-    }
-
-    Runnable StatusChecker = new Runnable() {
-        @Override
-        public void run() {
-            try {
-                updateStatus();
-            } finally {
-                if (mAutoUpdate) mRunHandler.postDelayed(StatusChecker, mUpdateInterval);
-            }
+            if (mActiveMarker != null)
+                mMap.animateCamera(CameraUpdateFactory.newLatLng(mActiveMarker.getPosition()));
         }
     };
 
-    void startRepeatingTask() {
-        StatusChecker.run();
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences sharedPref_ = PreferenceManager.getDefaultSharedPreferences(this);
+        mMyId = sharedPref_.getString("pref_name", "User");
+        if (mMyMarker != null) mMyMarker.setTitle(mMyId);
+        registerReceiver(broadcastReceiver, new IntentFilter(LocationService.str_receiver));
     }
 
-    void sendLocation(String location) {
-        if (mMyId.equals("User")) return;
-        byte[] encoded_id = new byte[0];
-        byte[] encoded_location = new byte[0];
-        try {
-            encoded_id = LocationSecurity.encrypt(mBfPassword, mMyId);
-            encoded_location = LocationSecurity.encrypt(mBfPassword, location);
-        } catch (GeneralSecurityException e) {
-            e.printStackTrace();
-        }
-        String url = mServerName + "?v1=" + LocationSecurity.byteArrayToHexString(encoded_id) + "&&v2=" + LocationSecurity.byteArrayToHexString(encoded_location);
-
-        StringRequest request = new StringRequest(com.android.volley.Request.Method.GET, url,
-                new com.android.volley.Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // No action
-                    }
-                }, new com.android.volley.Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                // No action
-            }
-        });
-        mRequestQueue.add(request);
-    }
-
-    void getLocation() {
-
-        String url = mServerName + "?v1=query";
-
-        Response.Listener<JSONArray> listener = new Response.Listener<JSONArray>() {
-            @Override
-            public void onResponse(JSONArray response) {
-                for (int i = 0; i < response.length(); i++) {
-                    try {
-                        JSONObject object = response.getJSONObject(i);
-
-                        String decodedID = "";
-                        String decodedLocation = "";
-                        try {
-                            decodedID = LocationSecurity.decrypt(mBfPassword, LocationSecurity.hexStringToByteArray(object.getString("N")));
-                            decodedLocation = LocationSecurity.decrypt(mBfPassword, LocationSecurity.hexStringToByteArray(object.getString("C")));
-                        } catch (GeneralSecurityException e) {
-                            e.printStackTrace();
-                        }
-
-                        if (!decodedID.equals(mMyId)) {
-                            Log.d("Record: ", "N: " + decodedID + " C: " + decodedLocation);
-                            String[] data = decodedLocation.split(";");
-                            Marker marker = mMarkers.get(decodedID);
-                            if (marker == null) {
-                                mMarkers.put(decodedID, mMap.addMarker(new MarkerOptions()
-                                        .position(new LatLng(Double.valueOf(data[0]), Double.valueOf(data[1])))
-                                        .title(decodedID)
-                                        .icon(BitmapDescriptorFactory.defaultMarker(MarkerProperties.MarkerColourByIndex(i)))));
-                            }
-                            else {
-                                marker.setPosition(new LatLng(Double.valueOf(data[0]), Double.valueOf(data[1])));
-                            }
-                        }
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        };
-
-        Response.ErrorListener error_listener = new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                // TODO: Handle error
-            }
-        };
-
-        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, "", listener, error_listener);
-        mRequestQueue.add(request);
-
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(broadcastReceiver);
     }
 }
